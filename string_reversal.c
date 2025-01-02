1f63486d033a174293a677ea8020992aecf31147
enzo253 <139024939+enzo253@users.noreply.github.com>
2025-01-03 00:13:02 +0100

    string reversal problem update 02

--- string_reversal.c
+++ string_reversal.c
@@ -3,10 +3,21 @@
 
 void string_reverse() {
 	int i;
-	i = 0;
-	
-	printf("choose your words wisely: ");
+	int j;
+	char str[100];
+	char temp;
 
+	i = 0;
+	j = 0;
+
+	printf("choose your words wisely: ");
+	scanf("reversed word: ", str);
+
+	for (i > str[i]; 1++) {
+		temp = str[i];
+		str[i] = str[j];
+		str[j] = temp;
+	}
 
 }
 
