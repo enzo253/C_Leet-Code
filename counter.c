399a05d4adb159601d7c91fb2efc95136503af37
enzo253 <139024939+enzo253@users.noreply.github.com>
2025-01-04 23:56:25 +0100

    practiced counter very easy

---
+++ counter.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void shitjava() {
+	int i;
+
+	i = 10;
+
+	while (i < 13) {
+	i++;
+	printf("%d\n", i);
+	}
+	
+}
+
+int main() {
+	shitjava();
+	return 0;
+}
